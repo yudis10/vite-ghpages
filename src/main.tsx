5b25b59be8405c574c8ae83fe06376403eaba974
Yudis <yudis@Yudis-MacBook-Pro.local>
2025-03-03 08:04:33 +0700

    added hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createRouter,
+  createHashHistory,
+} from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const hashHistory = createHashHistory();
+const router = createRouter({
+  routeTree,
+  history: hashHistory,
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -21,7 +29,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} basepath="vite-ghpages" />
+      <RouterProvider router={router} />
     </StrictMode>
   );
 }
